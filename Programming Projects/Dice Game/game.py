5ad58303501aeea9d6d4da074d7a9bd0c4d7d4ac
KinimodD <97245998+KinimodD@users.noreply.github.com>
2022-02-07 09:25:40 +0000

    Update game.py

--- Programming Projects/Dice Game/game.py
+++ Programming Projects/Dice Game/game.py
@@ -1,9 +1,18 @@
+from msilib.schema import TextStyle
 from tkinter import *
+import random
+from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Dice Game")
 
+player = 1
+turn = Label(root, text=("Player", str(player) + "'s", "turn"))
+turn.pack()
 
+
+roll = Button(root, text="Roll dice")
+roll.pack()
 
 
 
